b7aa58d3f399e90c7828c44f7982ecb264a08c0e
andchir <andchir@gmail.com>
2019-08-23 14:33:51 +0300

    FormIt integration

--- core/components/shopping_cart/elements/snippets/snippet.shopping_cart.php
+++ core/components/shopping_cart/elements/snippets/snippet.shopping_cart.php
@@ -1,6 +1,10 @@
 <?php
 
-/** @var array $scriptProperties */
+/*
+ * array $scriptProperties
+ * @var FormIt $formit
+ * @var fiHooks $hook
+ */
 
 $shoppingCart = $modx->getService(
     'shopping_cart',
@@ -8,7 +12,17 @@ $shoppingCart = $modx->getService(
     $modx->getOption('shopping_cart.core_path',null,$modx->getOption('core_path') . 'components/shopping_cart/') . 'model/shopping_cart/',
     $scriptProperties
 );
-if (!($shoppingCart instanceof ShoppingCart)) return '';;
+if (!($shoppingCart instanceof ShoppingCart)) return '';
+
+
+if (isset($formit) && $formit instanceof FormIt) {
+
+    //var_dump($formit->config); exit;
+    // TODO: Create orderOutputData option for FromIt
+    // $hook->setValues($orderOutputData);
+
+    return true;
+}
 
 $shoppingCart->updateConfig($scriptProperties);
 $output = $shoppingCart->actionResponse();
