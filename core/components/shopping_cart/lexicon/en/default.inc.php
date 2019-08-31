d1b6c7e7982ce3892adceab8325936f5388aa912
andchir <andchir@gmail.com>
2019-08-31 22:47:37 +0300

    shopping cart lexicons files

---
+++ core/components/shopping_cart/lexicon/en/default.inc.php
@@ -0,0 +1,26 @@
+<?php
+
+$_lang['shopping_cart'] = 'Shopping Cart';
+$_lang['shopping_cart.clear'] = 'Clear';
+$_lang['shopping_cart.recount'] = 'Recount';
+$_lang['shopping_cart.continue'] = 'Continue';
+$_lang['shopping_cart.total'] = 'Total';
+$_lang['shopping_cart.empty'] = 'Empty';
+$_lang['shopping_cart.checkout'] = 'Checkout';
+$_lang['shopping_cart.selected_items'] = 'Selected items';
+$_lang['shopping_cart.totaling'] = 'totaling';
+$_lang['shopping_cart.declension_products'] = 'product|products';
+$_lang['shopping_cart.full_name'] = 'Full name';
+$_lang['shopping_cart.address'] = 'Address';
+$_lang['shopping_cart.email'] = 'Email';
+$_lang['shopping_cart.phone'] = 'Phone';
+$_lang['shopping_cart.delivery_method'] = 'Delivery method';
+$_lang['shopping_cart.payment_method'] = 'Payment method';
+$_lang['shopping_cart.comment'] = 'Comment';
+$_lang['shopping_cart.submit'] = 'Submit';
+$_lang['shopping_cart.self_export'] = 'Self export';
+$_lang['shopping_cart.postal_service'] = 'Postal service';
+$_lang['shopping_cart.transport_company'] = 'Transport company';
+$_lang['shopping_cart.bank_card'] = 'Bank card';
+$_lang['shopping_cart.electronic_money'] = 'Electronic money';
+$_lang['shopping_cart.payment_upon_receipt'] = 'Payment upon receipt';
---
+++ core/components/shopping_cart/lexicon/ru/default.inc.php
@@ -0,0 +1,26 @@
+<?php
+
+$_lang['shopping_cart'] = 'Корзина покупок';
+$_lang['shopping_cart.clear'] = 'Очистить';
+$_lang['shopping_cart.recount'] = 'Пересчитать';
+$_lang['shopping_cart.continue'] = 'Продолжить';
+$_lang['shopping_cart.total'] = 'Итого';
+$_lang['shopping_cart.empty'] = 'Пусто';
+$_lang['shopping_cart.checkout'] = 'Оформить заказ';
+$_lang['shopping_cart.selected_items'] = 'Выбрано товаров';
+$_lang['shopping_cart.totaling'] = 'на сумму';
+$_lang['shopping_cart.declension_products'] = 'товар|товара|товаров';
+$_lang['shopping_cart.full_name'] = 'Ф.И.О.';
+$_lang['shopping_cart.address'] = 'Адрес';
+$_lang['shopping_cart.email'] = 'Адрес эл. почты';
+$_lang['shopping_cart.phone'] = 'Телефон';
+$_lang['shopping_cart.delivery_method'] = 'Способ доставки';
+$_lang['shopping_cart.payment_method'] = 'Способ оплаты';
+$_lang['shopping_cart.comment'] = 'Комментарий';
+$_lang['shopping_cart.submit'] = 'Отправить';
+$_lang['shopping_cart.self_export'] = 'Самовывоз';
+$_lang['shopping_cart.postal_service'] = 'Почта';
+$_lang['shopping_cart.transport_company'] = 'Транспортная компания';
+$_lang['shopping_cart.bank_card'] = 'Банковская карта';
+$_lang['shopping_cart.electronic_money'] = 'Электронные деньги';
+$_lang['shopping_cart.payment_upon_receipt'] = 'При получении';
